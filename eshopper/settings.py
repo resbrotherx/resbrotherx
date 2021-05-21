a430de9bcb4affca6815af64248ec31cc88a7a5a
IRES <francisdaniel140@gmail.com>
2021-05-21 16:26:26 -0700

    x

--- eshopper/settings.py
+++ eshopper/settings.py
@@ -166,10 +166,10 @@ USE_TZ = True
 
 
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-#     # os.path.join(BASE_DIR, 'staticfiles'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'staticfiles'),
+)
 
 
 STATIC_URL = '/static/'
