ed3bffd60c7f2e7f457ef698a97743585a52d7ef
IRES <francisdaniel140@gmail.com>
2021-05-21 15:59:28 -0700

    u

--- eshopper/settings.py
+++ eshopper/settings.py
@@ -175,7 +175,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'statics')
+    os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
